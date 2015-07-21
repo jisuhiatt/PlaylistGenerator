ac3d00e2abdb06206e0bb23f5657e485f026579c
Ji Su Park <parkh.jisu@gmail.com>
2015-07-21 10:39:38 -0500

    initial commit
    OAuth, HTML/CSS (materialize), profile information shows up under first
    card, sample AJAX code pulling Spotify API under first card (doesn’t
    work), sample personalized playlist

---
+++ app/js/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spotifyappApp
+ */
+angular.module('spotifyappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
